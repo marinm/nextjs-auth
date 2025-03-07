de9e02ac288f1a7677b14742b2ebb409d23aacc9
Marin <marinmema@live.ca>
2025-03-07 09:38:37 -0500

    use bootstrap icons web font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-    return "Home";
+    return (
+        <div>
+            <h1>Home</h1>
+            <button className="btn btn-primary">
+                <i className="bi bi-database"></i>
+                Database
+            </button>
+        </div>
+    );
 }
