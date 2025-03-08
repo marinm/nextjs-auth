9cf1f8f7e477e5b7a62118b69f0d05d1c3523bb5
Marin <marinmema@live.ca>
2025-03-07 19:27:14 -0500

    add benchmarks

--- src/hello.ts
+++ src/hello.ts
@@ -4,14 +4,17 @@ import Database from "better-sqlite3";
 dotenv.config();
 
 function main(): void {
+    console.time("db:connect");
     const db = new Database(process.env.DATABASE_URL);
     db.pragma("journal_mode = WAL");
+    console.timeEnd("db:connect");
 
     const [, , statement] = process.argv;
 
+    console.time("db:run");
     const prepared = db.prepare(statement);
-
     const result = prepared.reader ? prepared.all() : prepared.run();
+    console.timeEnd("db:run");
 
     console.log(result);
 
