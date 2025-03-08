f6355fc57853faac40b5a3ac15d0237d7d413c7c
Marin <marinmema@live.ca>
2025-03-08 11:59:32 -0500

    bootstrap form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,31 @@
 export default function Page() {
     return (
-        <div>
-            <form>
-                <input
-                    type="text"
-                    autoComplete="username"
-                    name="username"
-                    placeholder="Username"
-                />
-                <input
-                    type="password"
-                    autoComplete="password"
-                    name="password"
-                    placeholder="Password"
-                />
-                <button type="submit">Log in</button>
+        <div className="min-vh-100 d-flex flex-column justify-content-center">
+            <form className="d-grid gap-2">
+                <div className="form-floating">
+                    <input
+                        type="text"
+                        autoComplete="username"
+                        name="username"
+                        placeholder="Username"
+                        className="form-control"
+                    />
+                    <label>Username</label>
+                </div>
+                <div className="form-floating">
+                    <input
+                        type="password"
+                        autoComplete="password"
+                        name="password"
+                        placeholder="Password"
+                        className="form-control"
+                    />
+                    <label>Password</label>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-lg">
+                    Log in
+                </button>
             </form>
         </div>
     );
