1a720207550b592010b0a9502e2c3a7dfe86b619
Marin <marinmema@live.ca>
2025-03-09 18:21:43 -0400

    fix typo

--- src/app/user-provider.tsx
+++ src/app/user-provider.tsx
@@ -4,7 +4,7 @@ import { createContext } from "react";
 
 export const UserContext = createContext(null);
 
-export default function UseProvider({
+export default function UserProvider({
     children,
 }: {
     children: React.ReactNode;
