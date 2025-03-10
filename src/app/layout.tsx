f5b903fff45708f30e8ff7b7bae8f28e558763c4
Marin <marinmema@live.ca>
2025-03-10 09:11:23 -0400

    dont use user in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import UserContext from "@/app/UserContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
-import { sessionUser } from "./session-user";
 
 export const metadata: Metadata = {
     title: "Auth",
@@ -15,16 +13,9 @@ export default async function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const user = await sessionUser();
-    console.log(user);
-
     return (
         <html lang="en">
-            <body data-bs-theme="dark">
-                <UserContext.Provider value={user}>
-                    {children}
-                </UserContext.Provider>
-            </body>
+            <body data-bs-theme="dark">{children}</body>
         </html>
     );
 }
