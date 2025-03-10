976c3ffd7524dd4918d62d65fa45c1109e64a705
Marin <marinmema@live.ca>
2025-03-09 21:45:17 -0400

    export UserContext

--- src/app/UserContext.tsx
+++ src/app/UserContext.tsx
@@ -1,32 +1,16 @@
 "use client";
 
-import { useState, createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 import { User } from "@/data/users";
 
-export type UserContextType = {
-    user: null | User;
-    setUser: (user: null | User) => void;
-};
+const UserContext = createContext<null | User>(null);
 
-const UserContext = createContext<UserContextType | undefined>(undefined);
-
-export function useUser(): UserContextType {
+export function useUser(): User {
     const context = useContext(UserContext);
-    if (context === undefined) {
+    if (context === null) {
         throw new Error("useUser must be used within a UserProvider");
     }
     return context;
 }
 
-export default function UserProvider({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    const [user, setUser] = useState<null | User>(null);
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
-}
+export default UserContext;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import UserProvider from "@/app/UserContext";
+import UserContext from "@/app/UserContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
@@ -22,7 +22,9 @@ export default async function RootLayout({
         <html lang="en">
             <body data-bs-theme="dark">
                 <div className="container">
-                    <UserProvider>{children}</UserProvider>
+                    <UserContext.Provider value={user}>
+                        {children}
+                    </UserContext.Provider>
                 </div>
             </body>
         </html>
